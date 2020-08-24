0e7a9ee40fea273b8fc0e3d1a7bcee3e7127cbd4
Shakti <65832818+Shaktinetra@users.noreply.github.com>
2020-08-24 13:31:49 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 var dog, dogImg, happyDogImg, database, food, foodStock, button1, button2, fedTime, lastFed, foodObj;
 
 function preload() {
-  dogImg = loadImage("images/dogImg.png");
-  happyDogImg = loadImage("images/dogImg1.png");
+  dogImg = loadImage("dogImg.png");
+  happyDogImg = loadImage("dogImg1.png");
 }
 
 function setup() {
